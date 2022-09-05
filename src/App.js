0e9c4d9f9053856f4acdfa7662f5760462b9b7f3
craigmcc <cmcchesney@ospreydcs.com>
2022-09-05 15:16:57 -0600

    #1: establish grpc connection to datastore server and exercise basic API functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-const {QueryClient} = require('./grpc-proto/query_grpc_web_pb.js');
+const {Query, PVResponse} = require('./grpc-proto/query_pb.js');
+const {QueryServiceClient} = require('./grpc-proto/query_grpc_web_pb.js');
+
+console.log("connecting to datastore server");
+const client = new QueryServiceClient("http://localhost:8080");
+let queryString = "mpexPv09.*";
+let request = new Query(queryString);
+client.listPVs(request, {}, (err, response) => {
+  if (err) {
+    console.error("error encountered: " + err);
+    return;
+  }
+  console.log(response.getPvsList());
+});
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
