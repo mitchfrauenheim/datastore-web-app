c50d182a633e9c153549a1d357909de1c283df18
craigmcc <cmcchesney@ospreydcs.com>
2022-09-08 15:37:48 -0600

    #5: refactor web application, add QueryPage with PV count

--- src/App.js
+++ src/App.js
@@ -1,54 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+import QueryPage from "./pages/QueryPage";
+
 const {QueryServiceClient} = require('./grpc-proto/query_grpc_web_pb.js');
-const {Query, AnnotationResponse} = require('./grpc-proto/query_pb.js');
 
 console.log("connecting to datastore server");
-const client = new QueryServiceClient("http://localhost:8080");
+const client = new QueryServiceClient("http://localhost:8080", null, null);
 
-// annotation query
-let annotationQuery = ".*";
-let annotationRequest = new Query(annotationQuery);
-client.listAnnotations(annotationRequest, {}, (err, response) => {
-  if (err) {
-    console.error("error encountered in annotation query: " + err);
-    return;
+// // annotation query
+// let annotationQuery = ".*";
+// let annotationRequest = new Query(annotationQuery);
+// client.listAnnotations(annotationRequest, {}, (err, response) => {
+//   if (err) {
+//     console.error("error encountered in annotation query: " + err);
+//     return;
+//   }
+//   console.log("annotation result: ");
+//   console.log(response.getAnnotationsList());
+// });
+
+export default function App() {
+
+  function renderQueryPage() {
+      return <QueryPage client={client} />;
   }
-  console.log(response.getAnnotationsList());
-});
-
-// PV query
-let pvQuery = ".*Pv01";
-let pvRequest = new Query(pvQuery);
-client.listPVs(pvRequest, {}, (err, response) => {
-  if (err) {
-    console.error("error encountered in PV query: " + err);
-    return;
-  }
-  console.log(response.getPvsList());
-});
-
-function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the EPICS Data Platform.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <main className="main">
+        {renderQueryPage()}
+      </main>
     </div>
   );
 }
-
-export default App;
