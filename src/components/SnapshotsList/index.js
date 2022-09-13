cd7d3d2993e8100e00a06ef465ebaac9f68a9a90
craigmcc <cmcchesney@ospreydcs.com>
2022-09-12 18:03:02 -0600

    web app #7: use table on SnapshotPage to display data

--- src/components/SnapshotsList/index.js
+++ src/components/SnapshotsList/index.js
@@ -3,15 +3,22 @@ import "./SnapshotsList.css";
 export default function SnapshotsList({ snapshots = [], onOpen }) {
 
     return (
-        <div className="snapshotslist">
-            {snapshots?.map((snapshot, i) => {
-                return <SnapshotCard snapshot={snapshot} key={i} onOpen={onOpen}/>;
-            })}
-        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <th>id</th>
+                    <th>timestamp</th>
+                    <th>description</th>
+                </tr>
+                {snapshots?.map((snapshot, i) => {
+                    return <Snapshot snapshot={snapshot} key={i} onOpen={onOpen}/>;
+                })}
+            </tbody>
+        </table>
     );
 }
 
-function SnapshotCard({ snapshot, onOpen }) {
+function Snapshot({ snapshot, onOpen }) {
 
     function handleOpen() {
         console.log("open snapshot");
@@ -19,21 +26,11 @@ function SnapshotCard({ snapshot, onOpen }) {
     }
 
     return (
-        <div className="snapshotcard">
-            <div>
-                <div className="snapshotcard-column">
-                    <h3>{snapshot?.id || "No id"}</h3>
-                </div>
-                <div className="snapshotcard-column">
-                    <h3>{snapshot?.timestamp || ""}</h3>
-                </div>
-                <div className="snapshotcard-column">
-                    <h3>{snapshot?.description || ""}</h3>
-                </div>
-                <div>
-                    <button onClick={handleOpen}>Open</button>
-                </div>
-            </div>
-        </div>
+        <tr>
+            <td><h3>{snapshot?.id || "No id"}</h3></td>
+            <td><h3>{snapshot?.timestamp || ""}</h3></td>
+            <td><h3>{snapshot?.description || ""}</h3></td>
+            <td><button onClick={handleOpen}>Open</button></td>
+        </tr>
     );
 }
