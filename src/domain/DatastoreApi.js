11e01287771755f444236384e21e1a399b297fa0
craigmcc <cmcchesney@ospreydcs.com>
2022-09-30 11:56:01 -0600

    web app #24: add snapshot data filter edit and criteria display panels on snapshot view

--- src/domain/DatastoreApi.js
+++ src/domain/DatastoreApi.js
@@ -135,6 +135,36 @@ class DatastoreApi {
         });
     }
 
+    queryListSnapshotDataUsingFilter(filter, resultHandler, errorHandler) {
+        console.log("DatastoreApi.queryListSnapshotDataUsingFilter()");
+//
+//         // execute query to retrieve snapshot data
+//
+//         console.log("executing grpc snapshot data query");
+// //        let firstTimeString = new Date(firstSeconds*1000).toISOString();
+// //        let lastTimeString = new Date(lastSeconds*1000).toISOString();
+//         let firstTimeString = "2022-09-21T03:03:19.504Z";
+//         let lastTimeString = "2022-09-21T03:03:19.514Z";
+//         let queryString = "SELECT `*.*` WHERE time >= '" + firstTimeString + "' AND time <= '" + lastTimeString + "'";
+//         console.log(queryString);
+//
+//         const {
+//             Query
+//         } = require('../../grpc-proto/query_pb.js');
+//
+//         let snapshotQuery = new Query();
+//         snapshotQuery.setQuery(queryString);
+//         client.listSnapshotData(snapshotQuery, {}, (err, response) => {
+//             if (err) {
+//                 const errorMsg = response.getMsg();
+//                 console.log("error executing snapshot data query: " + errorMsg);
+//             } else {
+//                 console.log("snapshot data query success");
+//                 setSnapshotDataPage(new SnapshotDataPage(response));
+//             }
+//         });
+    }
+
 }
 
 module.exports = DatastoreApi
--- src/pages/ViewSnapshotPage/SnapshotDataPanel.js
+++ src/pages/ViewSnapshotPage/SnapshotDataPanel.js
@@ -1,12 +1,31 @@
 import React from "react";
 import SnapshotDataTable from "./SnapshotDataTable";
 
-export default function SnapshotDataPanel({ snapshotDataPage }) {
+export default function SnapshotDataPanel({ snapshotDataPage, errorMsg }) {
+
+    function renderQueryResultsPanel() {
+        return (
+            <div>
+                <div>
+                    <div style={{paddingBottom: "4px", borderBottom: "1px solid darkgray"}}>
+                        <SnapshotDataTable snapshotDataPage={snapshotDataPage}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    function renderQueryErrorPanel() {
+        return (
+            <div>
+                <h1>{errorMsg}</h1>;
+            </div>
+        );
+    }
 
     return (
         <div style={{paddingBottom: "4px", borderBottom: "1px solid darkgray"}}>
-            <SnapshotDataTable snapshotDataPage={snapshotDataPage}/>
+            {(errorMsg !== null) ? renderQueryErrorPanel() : renderQueryResultsPanel()}
         </div>
     );
-
 }
