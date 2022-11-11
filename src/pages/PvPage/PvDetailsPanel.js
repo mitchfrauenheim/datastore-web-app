660167bed5df1e5146ed70753ffb5a6f93f7e88d
craigmcc <auto@craigmcc.com>
2022-11-11 07:06:32 -0700

    datastore-web-app #63: add link for navigating from PvPage to SnapshotListPage with URL search parameter for specified PV

--- src/pages/PvPage/PvDetailsPanel.js
+++ src/pages/PvPage/PvDetailsPanel.js
@@ -1,25 +1,33 @@
 import React from "react";
 import FieldNameTypeTable from "../common/FieldNameTypeTable";
 import AttributeNameTable from "../common/AttributeNameTable";
+import {Link} from "react-router-dom";
 
-export default function PvDetailsPanel({ pv }) {
+export default function PvDetailsPanel({pv}) {
 
-    return (
-        <div style={{paddingBottom: "4px", borderBottom: "1px solid darkgray"}}>
-            <h1>PV Details</h1>
-            <b>Name: </b><p>{pv.name}</p>
-            <b>First Time: </b><p>{pv.firstTimestampLocaleString}</p>
-            <b>Last Time: </b><p>{pv.lastTimestampLocaleString}</p>
-            <b>Provider ID: </b><p>{pv.providerId}</p>
-            <b>Field Names and Types:</b>
-            <p/>
-            <FieldNameTypeTable objectWithFields={pv}/>
-            <p/>
-            <b>Attributes: </b>
-            <p/>
-            <AttributeNameTable objectWithAttributes={pv}/>
-            <p/>
-        </div>
-    );
+        return (
+            <div style={{paddingBottom: "4px", borderBottom: "1px solid darkgray"}}>
+                    <h1>PV Details</h1>
+                    <b>Name: </b>
+                    <p>
+                            {pv.name + ' '}
+                            <Link to={`/snapshotList?pvPattern1=${pv.name}`}>
+                                    (view snapshots)
+                            </Link>
+                    </p>
+                    <b>First Time: </b><p>{pv.firstTimestampLocaleString}</p>
+                    <b>Last Time: </b><p>{pv.lastTimestampLocaleString}</p>
+                    <b>Provider ID: </b><p>{pv.providerId}</p>
+                    <div></div>
+                    <b>Field Names and Types:</b>
+                    <p/>
+                    <FieldNameTypeTable objectWithFields={pv}/>
+                    <p/>
+                    <b>Attributes: </b>
+                    <p/>
+                    <AttributeNameTable objectWithAttributes={pv}/>
+                    <p/>
+            </div>
+        );
 
 }
