61addc2ab52d1d124acf347c3f9e1371dea38356
Mitch Frauenheim <mitch.frauenheim@wustl.edu>
2023-02-03 19:21:46 -0800

    Navbar
    Early implementation of the navbar, without custom styling

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,9 +1,146 @@
 import { Outlet, Link } from "react-router-dom";
+import * as React from 'react';
 
-const Layout = ({datastoreConfig}) => {
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const navLinks = [
+    {
+        name: "Data Explorer Home",
+        path: "/"
+    },
+    {
+        name: "Explore Snapshots",
+        path: "/snapshotList"
+    },
+    {
+        name: "Explore PVs",
+        path: "/pvList"
+    },
+    {
+        name: "Explore Annotations",
+        path: "/annotationList"
+    }
+]
+
+const drawerWidth = 240;
+
+// const Layout = ({datastoreConfig}) => {
+function Layout(props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawer = (
+        <div>
+            <Toolbar />
+            <Divider />
+            <List>
+                {navLinks.map(link => (
+                    <ListItem key={link.name} component={Link} to={link.path} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <InboxIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={link.name} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
     return (
         <>
-            <nav style={{paddingBottom: "4px", borderBottom: "1px solid darkgray"}}>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    sx={{
+                        width: { sm: `calc(100% - ${drawerWidth}px)` },
+                        ml: { sm: `${drawerWidth}px` }
+                    }}
+                >
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            sx={{ mr: 2, display: { sm: 'none' } }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" noWrap component="div">
+                            Osprey DCS
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Box
+                    component="nav"
+                    sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                    aria-label="navigation links"
+                >
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        ModalProps={{
+                            keepMounted: true
+                        }}
+                        sx={{
+                            display: { xs: 'block', sm: 'none' },
+                            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+                        }}
+                    >
+                        {drawer}
+                    </Drawer>
+                    <Drawer
+                        variant="permanent"
+                        sx={{
+                            display: { xs: 'none', sm: 'block' },
+                            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+                        }}
+                        open
+                    >
+                        {drawer}
+                    </Drawer>
+                </Box>
+                <Box
+                    component="main"
+                    sx={{ flexGrow: 1, pt: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+                >
+                    {/* <Toolbar /> */}
+                    <Typography paragraph>
+                        Hello World
+                    </Typography>
+
+                    <Outlet />
+                </Box>
+            </Box>
+
+            {/* <nav style={{paddingBottom: "4px", borderBottom: "1px solid darkgray"}}>
                 <ul>
                     <li>
                         <Link to="/">Data Explorer Home</Link>
@@ -18,10 +155,10 @@ const Layout = ({datastoreConfig}) => {
                         <Link to="/annotationList">Explore Annotations</Link>
                     </li>
                 </ul>
-                <p><i>Datastore configuration: {datastoreConfig}</i></p>
+                <p><i>Datastore configuration: {props.datastoreConfig}</i></p>
             </nav>
 
-            <Outlet />
+            <Outlet /> */}
         </>
     )
 };
